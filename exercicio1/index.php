1fe2e6fe1096833767d6e1298ff2624312c4ee38
Guilherme Kodama <guilherme.kodama@gmail.com>
2015-05-06 16:42:32 -0400

    Commit inicial

---
+++ exercicio1/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+	<title>Página de Teste</title>
+</head>
+<body>
+
+
+<h1>Instituto de Computação</h1>
+
+<table border="1">
+	<thead>
+		
+		<tr>
+			<th>asdadadd</th>
+			<th>adasdada</th>
+			<th>adsaadasa</th>
+
+		</tr>
+
+	</thead>
+
+	<tr>
+			<td>asdadadd</td>
+			<td>adasdada</td>
+			<td>adsaadasa</td>
+
+	</tr>
+
+		<tr>
+			<td>asdadadd</td>
+			<td>adasdada</td>
+			<td>adsaadasa</td>
+
+		</tr>
+</table>
+
+<audio autoplay="autoplay" controls="controls">
+	<source src="" type="audio/mpeg">
+	<source src="" type="audio/ogg">
+	Your broser does not support the audio tag.
+</audio>
+
+<button id="play">Play</button>
+<video id="protesto" src="" width="640" height="480" controls="controls">
+</video>
+<h1>FORMULÁRIO</h1>
+<form action="processa.php" method="get">
+<input type="text" name="nome_completo">
+<input type="submit" name="submit" value="Enviar">
+</form>
+
+</body>
+<script src="script.js" type="text/javascript"></script>
+</html>
