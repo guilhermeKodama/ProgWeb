c09b020904db342dc967d30875f02f3d5d8d2a10
Guilherme Kodama <guilherme.kodama@gmail.com>
2015-05-15 17:38:00 -0400

    Implementei 3 exercicios utilizando CSS

---
+++ html/exercicio-css-2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Exercicio 2 css</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<p class="box"> O Instituto de Computação é o mais novo instituto da UFAM , tendo sido formado a partir do antigo
+ Departamento de Ciência da Computação (DCC)</p>
+
+</body>
+</html>
---
+++ html/exercicio-css-3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Exercicio 2 css</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+
+<div class="box" id="box">O Instituto de Computação é o mais novo instituto da UFAM , tendo sido formado a partir do antigo
+ Departamento de Ciência da Computação (DCC)</div>
+
+</body>
+</html>
