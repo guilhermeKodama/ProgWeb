087514f6dbf8c903b165a28496c3dc2339412f24
Guilherme Kodama <guilherme.kodama@gmail.com>
2015-08-03 08:09:44 -0400

    Realizei o exercício 11

--- php/yii2/views/site/index.php
+++ php/yii2/views/site/index.php
@@ -1,11 +1,12 @@
 <?php
 /* @var $this yii\web\View */
 $this->title = 'My Yii Application';
+use yii\helpers\Html;
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <?= Html::img('@web/images/logo.png') ?><h1>Instituto de Computação</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
